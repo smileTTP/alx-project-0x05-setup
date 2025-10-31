385de2cbba37de941895bffca967f99e3d64e867
SmileTTP <ibtissamlazraf@gmail.com>
2025-10-31 21:37:24 +0100

    Track images generated

--- alx-project-0x11/components/common/ImageCard.tsx
+++ alx-project-0x11/components/common/ImageCard.tsx
@@ -2,10 +2,10 @@ import { GeneratedImageProps } from "@/interfaces";
 
 const ImageCard: React.FC<GeneratedImageProps> = ({ imageUrl, prompt, width, action }) => {
     return (
-    <div onClick={() => action(imageUrl)} className="mt-6 border hover:cursor-pointer">
+    <div onClick={() => action(imageUrl)} className="mt-6 bg-gray-700 border rounded-lg border-gray-300 hover:cursor-pointer">
         <img src={imageUrl} alt={prompt} className={`w-full max-w-md rounded-lg shadow-lg`} />
-        <h2 className={`${width ? 'text-sm' : 'text-xl'} font-semibold mt-2`}>Your Prompt:</h2>
-        <p className={`${width ? 'text-xs' : 'text-lg'} text-gray-700 mb-4`}>{prompt}</p>
+        <h2 className={`${width ? 'text-sm' : 'text-xl'} font-semibold mt-2 text-white`}>Your Prompt:</h2>
+        <p className={`${width ? 'text-xs' : 'text-lg'} text-gray-100 mb-4`}>{prompt}</p>
     </div>
     );
 }
